18b03bffda47473065aabca309e4fec8310314ad
Siwan Yang <syang00129@gmail.com>
2024-05-21 13:20:15 -0400

    Added try/Except

--- my_abs.py
+++ my_abs.py
@@ -1,4 +1,13 @@
+import math
+
 def my_abs(x):
+    try:
+        if x < 0:
+            return -x
+        else:
+            return x
+    except TypeError:
+        return math.nandef my_abs(x):
     if x < 0:
         return -x
     else:
