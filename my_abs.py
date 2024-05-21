3fdd39dfb9931dee173e9548b67e928085acd417
Siwan Yang <syang00129@gmail.com>
2024-05-21 13:29:48 -0400

    Using Type checks

--- my_abs.py
+++ my_abs.py
@@ -1,4 +1,17 @@
 import math
+import numbers
+
+def my_abs(x):
+    if isinstance(x, numbers.Real):
+        if x < 0:
+            return -x
+        else:
+            return x
+    elif isinstance(x, numbers.Complex):
+        return math.sqrt(
+            x.real ** 2 + x.imag ** 2)
+    else:
+        return math.nanimport math
 
 def my_abs(x):
     try:
